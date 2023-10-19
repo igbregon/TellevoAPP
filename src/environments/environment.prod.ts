0ae6cb9bcec75c209bbc5f8035151dcae7fc79f4
tomas.pvm.33@gmail.com <tomas.pvm.33@gmail.com>
2023-10-19 15:44:58 -0300

    Uso de appiRest, solucion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,18 @@
+// export const environment = {
+//   firebaseConfig: {
+//     projectId: 'ps-tellevoapp',
+//     appId: '1:649198696713:web:0e8c028a8d555fe8497bd8',
+//     storageBucket: 'ps-tellevoapp.appspot.com',
+//     apiKey: 'AIzaSyDwgobYdJjenZC8gnSkS8x04ZnmsA3WZFE',
+//     authDomain: 'ps-tellevoapp.firebaseapp.com',
+//     messagingSenderId: '649198696713',
+//     measurementId: 'G-NZ4XDC3EKT',
+//   },
+//   production: true
+// };
+
 export const environment = {
+  production: true,
   firebaseConfig: {
     projectId: 'ps-tellevoapp',
     appId: '1:649198696713:web:0e8c028a8d555fe8497bd8',
@@ -8,5 +22,12 @@ export const environment = {
     messagingSenderId: '649198696713',
     measurementId: 'G-NZ4XDC3EKT',
   },
-  production: true
+  firebaseConfig2: {
+    apiKey: "AIzaSyDBqn4rPVI24RGj_nZssA3GgblE7ZqIGMk",
+    authDomain: "pgy4121-003d-villacura.firebaseapp.com",
+    projectId: "pgy4121-003d-villacura",
+    storageBucket: "pgy4121-003d-villacura.appspot.com",
+    messagingSenderId: "311134375389",
+    appId: "1:311134375389:web:aa1704fa666fc12ccf3a34"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,19 @@
 
+// export const environment = {
+//   production: false,
+//   firebaseConfig: {
+//     projectId: 'ps-tellevoapp',
+//     appId: '1:649198696713:web:0e8c028a8d555fe8497bd8',
+//     storageBucket: 'ps-tellevoapp.appspot.com',
+//     apiKey: 'AIzaSyDwgobYdJjenZC8gnSkS8x04ZnmsA3WZFE',
+//     authDomain: 'ps-tellevoapp.firebaseapp.com',
+//     messagingSenderId: '649198696713',
+//     measurementId: 'G-NZ4XDC3EKT',
+//   },
+  
+// };
+
+
 export const environment = {
   production: false,
   firebaseConfig: {
@@ -10,4 +25,21 @@ export const environment = {
     messagingSenderId: '649198696713',
     measurementId: 'G-NZ4XDC3EKT',
   },
+  firebaseConfig2: {
+    apiKey: "AIzaSyDBqn4rPVI24RGj_nZssA3GgblE7ZqIGMk",
+    authDomain: "pgy4121-003d-villacura.firebaseapp.com",
+    projectId: "pgy4121-003d-villacura",
+    storageBucket: "pgy4121-003d-villacura.appspot.com",
+    messagingSenderId: "311134375389",
+    appId: "1:311134375389:web:aa1704fa666fc12ccf3a34"
+  },
+  apiUrl: "https://dev.matiivilla.cl/duoc/location/"
 };
+
+
+
+
+
+
+
+
